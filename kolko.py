033271e39223462fbf62952558af69348893af0a
mrzozolewski <zozol555577@gmail.com>
2023-05-07 17:26:21 +0200

    funkcja tworzaca nam plansze

--- kolko.py
+++ kolko.py
@@ -1,1 +1,12 @@
-print("Test")
+plansza = ["-","-","-",
+          "-","-","-",
+            "-","-","-",]
+gracz = "X"
+Wygrany = None
+
+def rysujplansze(plansza):
+    print(plansza[1] + " | " + plansza[2] + " | "+ plansza[3])
+    print(plansza[4] + " | " + plansza[5] + " | "+ plansza[6])
+    print(plansza[7] + " | " + plansza[8] + " | "+ plansza[9])
+
+rysujplansze(plansza)
