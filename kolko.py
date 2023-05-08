dcea73f5f0faad31c8d5d9fd1444d27e0d20d283
mrzozolewski <zozol555577@gmail.com>
2023-05-08 13:00:58 +0200

    Funkcja sprawdzajaca czy jest remis

--- kolko.py
+++ kolko.py
@@ -42,15 +42,15 @@ def wygrana_poziom(plansza):
     #wygrana w 1 rzedzie poziom ---
     if plansza[0] == plansza[1] == plansza[2]and plansza[0] != "-":
         wygrany = plansza[1] # nie ma to znaczenia jaki indeks z tablicy bedzie miala zmienna wygrany bo musza byc one rowne (chodzi oczywiscie o 0,1,2 i w innych przykladach inne liczby)
-        print("Wygrał {wygrany}")
+        print("Wygrał "+wygrany) 
     
     elif plansza[3] == plansza[4] == plansza[5]and plansza[3] != "-":
         wygrany = plansza[4]
-        print("Wygrał {wygrany}")
+        print("Wygrał "+wygrany) 
     
     elif plansza[6] == plansza[7] == plansza[8]and plansza[6] != "-":
         wygrany = plansza[7]
-        print("Wygrał {wygrany}")
+        print("Wygrał "+wygrany) 
     
     
 def wygrana_pion(plansza):
@@ -58,28 +58,38 @@ def wygrana_pion(plansza):
   
     if plansza[0] == plansza[3] == plansza[6]and plansza[0] != "-":
         wygrany = plansza[0]
-        print("Wygrał {wygrany}")
+        print("Wygrał "+wygrany) 
     
     elif plansza[1] == plansza[4] == plansza[7]and plansza[1] != "-":
         wygrany = plansza[4]
-        print("Wygrał {wygrany}")
+        print("Wygrał "+wygrany) 
     
     elif plansza[2] == plansza[5] == plansza[8]and plansza[2] != "-":
         wygrany = plansza[2]
-        print("Wygrał {wygrany}")
+        print("Wygrał "+wygrany) 
     
            
     
 def wygrana_na_ukos(plansza):
     global wygrany
-    
+    global gra_trwa
     if plansza[0] == plansza[4] == plansza[8] and plansza[0] != "-":
         wygrany = plansza[0]
-        print("Wygrał {wygrany}")    
+        print("Wygrał "+wygrany)
+        gra_trwa = False    
     elif plansza[2] == plansza[4] == plansza[6] and plansza[2] != "-":
         wygrany = plansza[4]
-        print("Wygrał {wygrany}")
-    
+        print("Wygrał "+wygrany)
+        gra_trwa = False 
+
+def remis(plansza):
+    global gra_trwa
+    if "-" not in plansza:
+        rysujplansze(plansza)
+        print("Remis")
+        gra_trwa = False
+
+        
 def zmiana_gracza():
     global aktualny_gracz 
     if aktualny_gracz== "X":
@@ -94,3 +104,4 @@ while gra_trwa == True:
     wygrana_na_ukos(plansza)
     wygrana_pion(plansza)    
     wygrana_poziom(plansza)
+    remis(plansza)
